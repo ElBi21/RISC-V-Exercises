256f61642ed44a548ab2ea0646ff6a3ff18d4fd8
Leonardo Biason <64312343+ElBi21@users.noreply.github.com>
2023-04-05 17:33:03 +0200

    Update riscv1-ex2.asm

--- Exercise 04/riscv1-ex2.asm
+++ Exercise 04/riscv1-ex2.asm
@@ -7,8 +7,8 @@
 	
 		lw x10, 0(x11)		# store the number
 	
-        ori x12, x10, 0		# temp store the number
-loop:	addi x12, x12, -1	# reduce the num of 1
+        	ori x12, x10, 0		# temp store the number
+loop:		addi x12, x12, -1	# reduce the num of 1
 		mul x10, x10, x12	# multiply the numbers
 		bgt x12, x17, loop	# repeat if bigger than 1
 	
